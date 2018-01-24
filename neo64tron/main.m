24cbe1d5d076ddfab504704386dedbf9f36ad067
Sam Anguiano <sam@anguiano.net>
2018-01-23 20:34:02 -0800

    Initial Commit

---
+++ neo64tron/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  neo64tron
+//
+//  Created by Sam Anguiano on 1/23/18.
+//  Copyright © 2018 dodecaplexor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
